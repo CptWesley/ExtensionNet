fa654e49add3d06d5f8e1bdb5226c31fa8597eba
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-10-20 16:25:28 +0200

    Add string copying

---
+++ src/ExtensionNet.Copy.Tests/CopyExtensionTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using static AssertNet.Xunit.Assertions;
+
+namespace ExtensionNet.Copy.Tests
+{
+    /// <summary>
+    /// Test class for the <see cref="CopyExtension"/> class.
+    /// </summary>
+    public static class CopyExtensionTests
+    {
+        /// <summary>
+        /// Checks that we can create a copy of a string.
+        /// </summary>
+        [Fact]
+        public static void StringTest()
+        {
+            string str = "hello world";
+            AssertThat(str.Copy()).IsEqualTo(str);
+        }
+    }
+}
---
+++ src/ExtensionNet.Copy/CopyExtension.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.Serialization;
+
+namespace ExtensionNet.Copy
+{
+    /// <summary>
+    /// Static extension class which adds a copy method to every object.
+    /// </summary>
+    public static class CopyExtension
+    {
+        /// <summary>
+        /// Creates a shallow copy of the object.
+        /// </summary>
+        /// <typeparam name="T">Type of the object.</typeparam>
+        /// <param name="that">The object of which to create a shallow copy.</param>
+        /// <returns>A shallow copy of the object.</returns>
+        public static T Copy<T>(this T that) => that.Copy(false);
+
+        /// <summary>
+        /// Creates a copy of the object.
+        /// </summary>
+        /// <typeparam name="T">Type of the object.</typeparam>
+        /// <param name="that">The object of which to create a copy.</param>
+        /// <param name="deep">Specifies whether or not the copy should be a deep copy.</param>
+        /// <returns>A copy of the object.</returns>
+        public static T Copy<T>(this T that, bool deep)
+        {
+            if (that is string)
+            {
+                return that;
+            }
+
+            T copy = (T)FormatterServices.GetUninitializedObject(typeof(T));
+            return copy;
+        }
+    }
+}
