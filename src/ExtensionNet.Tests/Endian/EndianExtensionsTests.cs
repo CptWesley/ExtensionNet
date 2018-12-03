b2d6b26b5c655a163014f9d12d2ac790404c9ad1
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-12-03 17:06:07 +0100

    Add endianness extensions to numbers

---
+++ src/ExtensionNet.Tests/Endian/EndianExtensionsTests.cs
@@ -0,0 +1,55 @@
+﻿using ExtensionNet.Endian;
+using Xunit;
+
+using static AssertNet.Xunit.Assertions;
+
+namespace ExtensionNet.Tests.Endian
+{
+    /// <summary>
+    /// Test class for the <see cref="EndianExtensions"/> class.
+    /// </summary>
+    public static class EndianExtensionsTests
+    {
+        /// <summary>
+        /// Checks that we can correctly invert the endianness of the value.
+        /// </summary>
+        [Fact]
+        public static void Int16ChangeEndiannessTest()
+            => AssertThat(((short)-234).ChangeEndianness()).IsEqualTo(5887);
+
+        /// <summary>
+        /// Checks that we can correctly invert the endianness of the value.
+        /// </summary>
+        [Fact]
+        public static void UInt16ChangeEndiannessTest()
+            => AssertThat(((ushort)67).ChangeEndianness()).IsEqualTo(17152);
+
+        /// <summary>
+        /// Checks that we can correctly invert the endianness of the value.
+        /// </summary>
+        [Fact]
+        public static void Int32ChangeEndiannessTest()
+            => AssertThat(((int)-78).ChangeEndianness()).IsEqualTo(-1291845633);
+
+        /// <summary>
+        /// Checks that we can correctly invert the endianness of the value.
+        /// </summary>
+        [Fact]
+        public static void UInt32ChangeEndiannessTest()
+            => AssertThat(((uint)34).ChangeEndianness()).IsEqualTo(570425344);
+
+        /// <summary>
+        /// Checks that we can correctly invert the endianness of the value.
+        /// </summary>
+        [Fact]
+        public static void Int64ChangeEndiannessTest()
+            => AssertThat(((long)128).ChangeEndianness()).IsEqualTo((long)1 << 63);
+
+        /// <summary>
+        /// Checks that we can correctly invert the endianness of the value.
+        /// </summary>
+        [Fact]
+        public static void UInt64ChangeEndiannessTest()
+            => AssertThat(((ulong)128).ChangeEndianness()).IsEqualTo((ulong)1 << 63);
+    }
+}
---
+++ src/ExtensionNet/Endian/EndianExtensions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace ExtensionNet.Endian
+{
+    /// <summary>
+    /// Class containing extension methods relating to endianness of numbers.
+    /// </summary>
+    public static class EndianExtensions
+    {
+        /// <summary>
+        /// Changes the endianness of the value.
+        /// </summary>
+        /// <param name="value">The value to change the endianness of.</param>
+        /// <returns>The value with endianness changed.</returns>
+        public static short ChangeEndianness(this short value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            Array.Reverse(bytes);
+            return BitConverter.ToInt16(bytes, 0);
+        }
+
+        /// <summary>
+        /// Changes the endianness of the value.
+        /// </summary>
+        /// <param name="value">The value to change the endianness of.</param>
+        /// <returns>The value with endianness changed.</returns>
+        public static ushort ChangeEndianness(this ushort value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            Array.Reverse(bytes);
+            return BitConverter.ToUInt16(bytes, 0);
+        }
+
+        /// <summary>
+        /// Changes the endianness of the value.
+        /// </summary>
+        /// <param name="value">The value to change the endianness of.</param>
+        /// <returns>The value with endianness changed.</returns>
+        public static int ChangeEndianness(this int value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            Array.Reverse(bytes);
+            return BitConverter.ToInt32(bytes, 0);
+        }
+
+        /// <summary>
+        /// Changes the endianness of the value.
+        /// </summary>
+        /// <param name="value">The value to change the endianness of.</param>
+        /// <returns>The value with endianness changed.</returns>
+        public static uint ChangeEndianness(this uint value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            Array.Reverse(bytes);
+            return BitConverter.ToUInt32(bytes, 0);
+        }
+
+        /// <summary>
+        /// Changes the endianness of the value.
+        /// </summary>
+        /// <param name="value">The value to change the endianness of.</param>
+        /// <returns>The value with endianness changed.</returns>
+        public static long ChangeEndianness(this long value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            Array.Reverse(bytes);
+            return BitConverter.ToInt64(bytes, 0);
+        }
+
+        /// <summary>
+        /// Changes the endianness of the value.
+        /// </summary>
+        /// <param name="value">The value to change the endianness of.</param>
+        /// <returns>The value with endianness changed.</returns>
+        public static ulong ChangeEndianness(this ulong value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            Array.Reverse(bytes);
+            return BitConverter.ToUInt64(bytes, 0);
+        }
+    }
+}
