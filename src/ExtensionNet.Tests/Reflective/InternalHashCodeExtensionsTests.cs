93242e953dd1f496efb472a545959533f19d3b27
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2018-12-26 11:57:57 +0100

    Add internal hash code extensions

---
+++ src/ExtensionNet.Tests/Reflective/InternalHashCodeExtensionsTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using ExtensionNet.Reflective;
+using Xunit;
+using static AssertNet.Xunit.Assertions;
+
+namespace ExtensionNet.Tests.Reflective
+{
+    /// <summary>
+    /// Test class for the <see cref="InternalHashCodeExtensionsTests"/> class.
+    /// </summary>
+    public static class InternalHashCodeExtensionsTests
+    {
+        /// <summary>
+        /// Checks that integers return the right hashcode.
+        /// </summary>
+        [Fact]
+        public static void IntegerTest()
+        {
+            AssertThat(42.GetInternalHashCode(true)).IsEqualTo(42.GetInternalHashCode(true));
+            AssertThat(42.GetInternalHashCode(true)).IsNotEqualTo(44.GetInternalHashCode(true));
+        }
+
+        /// <summary>
+        /// Checks that strings return the right hashcode.
+        /// </summary>
+        [Fact]
+        public static void StringTest()
+        {
+            AssertThat("Hello".GetInternalHashCode(true)).IsEqualTo("Hello".GetInternalHashCode(true));
+            AssertThat("Hello".GetInternalHashCode(true)).IsNotEqualTo("Bye".GetInternalHashCode(true));
+        }
+
+        /// <summary>
+        /// Checks that a more complex class returns the right hashcode.
+        /// </summary>
+        [Fact]
+        public static void ComplexTest()
+        {
+            Random a = new Random();
+            Random b = a.Copy(true);
+
+            AssertThat(a.GetInternalHashCode(true)).IsEqualTo(b.GetInternalHashCode(true));
+        }
+
+        /// <summary>
+        /// Checks that shallow hashcodes are correct.
+        /// </summary>
+        [Fact]
+        public static void CircularShallowTest()
+        {
+            DummyClasses.CircularClass a = new DummyClasses.CircularClass();
+            DummyClasses.CircularClass b = new DummyClasses.CircularClass();
+            a.Reference = b;
+            b.Reference = a;
+
+            DummyClasses.CircularClass c = a.Copy(false);
+
+            AssertThat(a.GetInternalHashCode(false)).IsEqualTo(c.GetInternalHashCode(false));
+            c.Reference = a;
+            AssertThat(a.GetInternalHashCode(false)).IsNotEqualTo(c.GetInternalHashCode(false));
+        }
+    }
+}
---
+++ src/ExtensionNet/Reflective/InternalHashCodeExtensions.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace ExtensionNet.Reflective
+{
+    /// <summary>
+    /// Static extension class adding methods to objects to retrieve a hashcode calculated using reflection.
+    /// </summary>
+    public static class InternalHashCodeExtensions
+    {
+        /// <summary>
+        /// Gets the internal hash code of the object using reflection.
+        /// </summary>
+        /// <param name="that">The object to get the hash code from.</param>
+        /// <returns>The internal hash code retrieved using reflection.</returns>
+        public static int GetInternalHashCode(this object that)
+            => that.GetInternalHashCode(false);
+
+        /// <summary>
+        /// Gets the internal hash code of the object using reflection.
+        /// </summary>
+        /// <param name="that">The object to get the hash code from.</param>
+        /// <param name="deep">Whether or not to get the hash code recursively.</param>
+        /// <returns>The internal hash code retrieved using reflection.</returns>
+        public static int GetInternalHashCode(this object that, bool deep)
+            => that.GetInternalHashCode(deep, new Dictionary<object, int>());
+
+        private static int GetInternalHashCode(this object that, bool deep, Dictionary<object, int> hashes)
+        {
+            if (that == null)
+            {
+                return 0;
+            }
+
+            Type type = that.GetType();
+            if (that is string || type.IsPrimitive)
+            {
+                return that.GetHashCode();
+            }
+
+            if (hashes.TryGetValue(that, out int hash))
+            {
+                return hash;
+            }
+
+            int result;
+            if (type.IsArray)
+            {
+                result = GetArrayHash(that as Array, deep, hashes);
+            }
+            else
+            {
+                result = GetObjectHash(that, deep, hashes);
+            }
+
+            hashes.Add(that, result);
+            return result;
+        }
+
+        private static int GetArrayHash(Array that, bool deep, Dictionary<object, int> hashes)
+        {
+            int result = that.GetType().GetHashCode();
+
+            for (int i = 0; i < that.Length; ++i)
+            {
+                object current = that.GetValue(i);
+                result += (i + 1) * (deep ? current.GetInternalHashCode(deep, hashes) : current.GetHashCode());
+            }
+
+            return result;
+        }
+
+        private static int GetObjectHash(object that, bool deep, Dictionary<object, int> hashes)
+        {
+            Type type = that.GetType();
+            int result = type.GetHashCode();
+            int scalar = 1;
+
+            while (type != null)
+            {
+                FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+                foreach (FieldInfo field in fields)
+                {
+                    result += (scalar++) * (deep ? field.GetValue(that).GetInternalHashCode(deep, hashes) : field.GetValue(that).GetHashCode());
+                }
+
+                type = type.BaseType;
+            }
+
+            return result;
+        }
+    }
+}
