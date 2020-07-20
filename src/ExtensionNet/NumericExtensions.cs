2834135d5804c0856f34e97916e97a316f7a7a9c
Wesley Baartman <w.j.baartman@student.tudelft.nl>
2020-07-20 15:51:43 +0200

    Add extensions for numeric values

---
+++ src/ExtensionNet/NumericExtensions.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ExtensionNet
+{
+    /// <summary>
+    /// Provides extension methods for numeric values.
+    /// </summary>
+    public static class NumericExtensions
+    {
+        /// <summary>
+        /// Gets the bytes of the value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="endianness">The endianness.</param>
+        /// <returns>The bytes representing the value.</returns>
+        public static byte[] GetBytes(this short value, Endianness endianness = Endianness.Current)
+            => BitConverter.GetBytes(value).SetEndianness(endianness);
+
+        /// <summary>
+        /// Gets the bytes of the value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="endianness">The endianness.</param>
+        /// <returns>The bytes representing the value.</returns>
+        public static byte[] GetBytes(this ushort value, Endianness endianness = Endianness.Current)
+            => BitConverter.GetBytes(value).SetEndianness(endianness);
+
+        /// <summary>
+        /// Gets the bytes of the value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="endianness">The endianness.</param>
+        /// <returns>The bytes representing the value.</returns>
+        public static byte[] GetBytes(this int value, Endianness endianness = Endianness.Current)
+            => BitConverter.GetBytes(value).SetEndianness(endianness);
+
+        /// <summary>
+        /// Gets the bytes of the value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="endianness">The endianness.</param>
+        /// <returns>The bytes representing the value.</returns>
+        public static byte[] GetBytes(this uint value, Endianness endianness = Endianness.Current)
+            => BitConverter.GetBytes(value).SetEndianness(endianness);
+
+        /// <summary>
+        /// Gets the bytes of the value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="endianness">The endianness.</param>
+        /// <returns>The bytes representing the value.</returns>
+        public static byte[] GetBytes(this long value, Endianness endianness = Endianness.Current)
+            => BitConverter.GetBytes(value).SetEndianness(endianness);
+
+        /// <summary>
+        /// Gets the bytes of the value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="endianness">The endianness.</param>
+        /// <returns>The bytes representing the value.</returns>
+        public static byte[] GetBytes(this ulong value, Endianness endianness = Endianness.Current)
+            => BitConverter.GetBytes(value).SetEndianness(endianness);
+    }
+}
